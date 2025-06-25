4c8244c418679c6a7a3895429bf659545e09138e
Sonu-2304 <yogeshsoni2304@gmail.com>
2025-06-25 17:11:46 +0530

    Added pyramid file
    This module contains code to create a pyramid, in the given range

---
+++ myPer.py
@@ -0,0 +1,8 @@
+def per():
+    a = int(input("Enter a:"))
+    b = int(input("Enter b:"))
+    for i in range(a,b):
+        for j in range(1,i+1):
+            print(j,end = ' ')
+        print()
+        print()
